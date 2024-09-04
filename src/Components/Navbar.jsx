27522d2172eb66e9935a14ffcd3873553ad3adc4
Mahi Singh <154801399+mahi13singh2004@users.noreply.github.com>
2024-09-05 02:07:24 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,12 +13,13 @@ const Navbar = () => {
       <div className="w-full h-16 flex justify-between items-center px-4 sticky top-0 z-50 bg-white shadow-md">
         <h1 className='tracking-widest font-bold text-2xl'>Careerदोस्त</h1>
         <div className="flex space-x-4 items-center">
-          <button
+          <a
+            href="https://contactus-weld.vercel.app/"
             className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white px-6 py-2 rounded-full shadow-lg hover:shadow-xl transition-shadow focus:outline-none focus:ring-2 focus:ring-purple-300 transform hover:scale-105 hover:rotate-2"
             onClick={addInfo}
           >
             Connect with Us
-          </button>
+          </a>
         </div>
       </div>
 
